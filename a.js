bbba6f0f230ecd96ff3b05986675147f4a63ff0d
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-20 19:32:45 +0700

    a

--- a.js
+++ a.js
@@ -1,5 +1,81 @@
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
+// 1 : khai bao bien let const var
+//pham vi hoat dong
+// console.log(name);
+// const name = "Nguyen Van a"
+// // let age = 10
+// // const address = 38
+// console.log(name)
+// var name = "nguyen van a";
+// if(true){
+//     var name = "nguyen van c";
+//     console.log(name)
+// }
+// console.log(name)
+//2 kieu du lieu : Symbol , String ,number , boolean , NaN , null
+// let a = 10;
+
+// let b = 5
+
+// // 6 gia tri la false : false , 0 , '' ,NaN ,undefined , null
+// if(-1){
+//     console.log("A lớn hơn B");
+// }else{
+//     console.log("A khác B");
+// }
+
+// let a = 5;
+// let b = 6
+// let c = ++a - ++b + a++ - b++ + a--
+// // 6 - ++b + a++ - b++ + a--
+// // 6 - 7 + a++ - b++ + a--
+// // 6 - 7 + 6 - b++ + a--
+// // 6 - 7 + 6 - 7 + 7
+// console.log(c);
+
+// undefined
+//1 khai bao khong truyen du lieu
+// var name
+// console.log(name);
+//2 phuong thuc khong co return 
+// function getname(){
+//     // return === return ; === return undefined
+// }
+// console.log(getname())
+//3 truy thuoc tinh khong ton tai
+// const a = {name : "Nguyen Van B"};
+// console.log(a.tuoi)
+//null
+// co tinh gan bang null
+//NaN
+//1 undefined + number
+// var a = 10;
+// var b
+// var c = a + b;
+// console.log(c);
+//2 undefined + undefined
+// var a ,b 
+// var c = a +b
+// console.log(c);
+//3 NaN + NaN
+// var a ,b = NaN
+// var c = a +b
+// console.log(c);
+// gia tri = 1 === true,
+// gia tri = 0 === false , null
+//Xu ly toan tu
+//Xem gia tri dau tien
+// var a = '200'
+// var b = '1000'
+// console.log(a > b)
+//try catch
+// try {
+//     var a = 10;
+//     var b = a.x.y 
+//     throw new Error("loi")
+//     console.log(a)
+// } catch (error) {
+//     console.log(error)
+// }
+// Random
+var random = Math.floor((Math.random() * 5) + 1);
+console.log(random);
