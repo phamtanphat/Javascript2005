70e5b61630df76a8d74447c95d539049bab6633c
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-24 19:25:40 +0700

    a

--- c.js
+++ c.js
@@ -62,12 +62,44 @@
 // console.log(getAllNameArr(mangperson));
 
 //filter
-const numbers = [1,2,3,4,5,6,7,8]
-console.log(numbers.filter(function(number){
-       if(number >= 3){
-           return true
-       }else{
-           return false
-       }
-}));
+// const numbers = [1,2,3,4,5,6,7,8]
+// console.log(numbers.filter(function(number){
+//        if(number >= 3){
+//            return true
+//        }else{
+//            return false
+//        }
+// }));
+// const numbers = [1,2,3,4,5,6,7,8]
+//function getArrsochan tra ve 1 mang moi 
+// các giá trị trong mảng mới phải là số chẳn
+// getArrsochan(numbers)
 
+// function getArrsochan(numbers){
+//     return numbers.filter(function(number){
+//         return number % 2 === 0 ? true : false
+//     })
+// }
+// console.log(getArrsochan(numbers))
+const mangperson = [
+    {name : 'Teo' , male : 'male'},
+    {name : 'Ti' , male : 'male'},
+    {name : 'Tun' , male : 'male'},
+    {name : 'Hoa', male : 'female'}
+]
+
+//function truyen vào 1 mảng và 1 giá trị cho giới tính
+// trả về 1 mảng mới và theo yêu cầu của giới tính truyền vào
+function getMale(arr , male){
+    let mangfiltered = arr.filter(function(person){
+        if(person.male === male){
+            return true
+        }else{
+            return false;
+        }
+        
+    });
+    console.log(mangfiltered);
+}
+
+getMale(mangperson , "female")
